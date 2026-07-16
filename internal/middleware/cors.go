fbaccc80b99fdc078b9f6b914be4c845889c1aaf
Andi <andy.u789@gmail.com>
2026-07-16 04:15:21 +0300

    asdasd

--- internal/middleware/cors.go
+++ internal/middleware/cors.go
@@ -5,7 +5,7 @@ import "github.com/gofiber/fiber/v3/middleware/cors"
 func NewCORS() cors.Config {
 	return cors.Config{
 		AllowOrigins:     []string{"https://find-vibe.vercel.app", "http://localhost:4200"},
-		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization", "ngrok-skip-browser-warning"},
 		AllowCredentials: true,
 		ExposeHeaders:    []string{"Content-Length"},
